d2e8c49597e23a6a7dd79c37ee8b313407d9f482
eyelady <keeganpatton@gmail.com>
2025-11-23 12:20:49 -0600

    set for push

--- lua/eyelady/plugins/colorscheme.lua
+++ lua/eyelady/plugins/colorscheme.lua
@@ -1,3 +1,26 @@
+--! CyberDream -->>
+-- return {
+--   "scottmckendry/cyberdream.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("cyberdream").setup({
+--       transparent = true,
+--       italic_comments = true,
+--       terminal_colors = true,
+--       hide_fillchars = true,
+--       borderless_telescope = true,
+--       theme = {
+--         saturation = 1,
+--       },
+--     })
+--     vim.cmd("colorscheme cyberdream")
+--   end,
+-- }
+--
+
+--! Flow -->>
+
 return {
   "0xstepit/flow.nvim",
   lazy = false,
